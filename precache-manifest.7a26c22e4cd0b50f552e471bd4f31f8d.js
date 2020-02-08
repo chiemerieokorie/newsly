33b340dbd467c5867ab50ffe60c18d8ae4900b9f
Chief <emerieokorie@gmail.com>
2020-02-08 16:57:04 +0100

    Updates

---
+++ precache-manifest.7a26c22e4cd0b50f552e471bd4f31f8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7538bacd5c3338bff37a5945dd6acf0",
+    "url": "/newsly/index.html"
+  },
+  {
+    "revision": "caead905a80cab2a4265",
+    "url": "/newsly/static/js/2.569545e0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/newsly/static/js/2.569545e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df78a3b61651c51eab24",
+    "url": "/newsly/static/js/main.1551fa0c.chunk.js"
+  },
+  {
+    "revision": "564cccd455afe6b40f88",
+    "url": "/newsly/static/js/runtime-main.48c7dc41.js"
+  }
+]);
